5d7e38be37694f92fc677711d99cb22a2da1a2a8
Vinícius Marini <marini_costa@hotmail.com>
2022-06-02 09:44:45 -0300

    urlimagem

--- mobile/farmverse/lib/model/animal.dart
+++ mobile/farmverse/lib/model/animal.dart
@@ -2,6 +2,7 @@
 class Animal {
   final String descricao;
   final String proprietario;
+  final String urlImagem;
   final num preco;
   final num id;
 
@@ -9,7 +10,8 @@ class Animal {
       {required this.id,
       required this.descricao,
       required this.proprietario,
-      required this.preco});
+      required this.preco,
+      required this.urlImagem});
 
   factory Animal.fromJson(Map<String, dynamic> json) {
     return Animal(
@@ -17,6 +19,7 @@ class Animal {
       descricao: json['descricao'],
       preco: json['preco'],
       proprietario: json['proprietario'],
+      urlImagem: json['urlimagem']
     );
   }
 }
